81eabf05d6ba969297b2afab3bab3a993dd76d1a
Gama_Ray <gamaizingg@gmail.com>
2023-12-20 20:21:24 +0300

    Практика по VUE

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# vue_practice_v1
+# vue_practice
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/components/UI/MyInput.vue
@@ -0,0 +1,30 @@
+<template>
+  <input
+      class="input"
+      :value="modelValue"
+      @input="updateInput"
+      type="text"
+      placeholder="Название">
+</template>
+
+<script>
+export default {
+  name: 'my-input',
+  props: {
+    modelValue: [String, Number]
+  },
+  methods: {
+    updateInput(event) {
+      this.$emit('update:modelValue', event.target.value)
+    }
+  }
+}
+</script>
+
+<style scoped>
+.input {
+  width: 100%;
+  border: 1px solid green;
+  padding: 10px 15px;
+}
+</style>
